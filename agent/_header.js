df81ccb8dbe2d83df1cb72db33b749829e54b413
admin <admin@admins-Mac-mini.local>
2014-09-22 18:26:18 +0530

    first_commit_2

---
+++ agent/_header.js
@@ -0,0 +1,35 @@
+// Locate your certificate
+var join = require('path').join
+  , pfx = join(__dirname, '../_certs/pfx.p12');
+
+// Create a new agent
+var apnagent = require('apnagent')
+  , agent = module.exports = new apnagent.Agent();
+
+
+  // set our credentials
+agent.set('pfx file', pfx);
+
+// our credentials were for development
+agent.enable('sandbox');
+
+
+agent.connect(function (err) {
+  // gracefully handle auth problems
+  if (err &amp; err.name === 'GatewayAuthorizationError') {
+    console.log('Authentication Error: %s', err.message);
+    process.exit(1);
+  }
+
+  // handle any other err (not likely)
+  else if (err) {
+    throw err;
+  }
+
+  // it worked!
+  var env = agent.enabled('sandbox')
+    ? 'sandbox'
+    : 'production';
+
+  console.log('apnagent [%s] gateway connected', env);
+});
